b45fdb4970949f79534556367404cff18f851dd3
Taha Tesser <48603081+TahaTesser@users.noreply.github.com>
2025-11-09 14:38:58 +0200

    Initial Commit

---
+++ Sauron/SauronApp.swift
@@ -0,0 +1,17 @@
+//
+//  SauronApp.swift
+//  Sauron
+//
+//  Created by Taha Tesser on 09.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SauronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
